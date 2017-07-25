272b54ab14402a89191c29015eff9aa28c9b477f
ali <ali@test.com>
2017-07-25 12:41:34 -0500

    Updated documentation dacorators

--- apiserver/core/data_models.py
+++ apiserver/core/data_models.py
@@ -1,3 +1,27 @@
+# Copyright (c) 2017, MD2K Center of Excellence
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 from flask_restplus import fields as rest_fields
 
 def stream_data_model(stream_api):
@@ -52,14 +76,29 @@ def stream_data_model(stream_api):
 
 def auth_data_model(stream_api):
     auth = stream_api.model('Authentication', {
-        'email_id': rest_fields.String(required=True),
+        'email_id': rest_fields.String(required=True, type="email"),
         'password': rest_fields.String(required=True),
     })
     return auth
 
-def objects_list_model(api):
-    model = api.model('Model', {
-        'name': rest_fields.String,
-        'address': rest_fields.String,
-        'date_updated': rest_fields.DateTime(dt_format='rfc822'),
+########################
+#   Response Models
+########################
+
+def error_model(api):
+    resp = api.model('error_model', {
+        'message': rest_fields.String
     })
+    return resp
+
+def auth_token_resp_model(api):
+    resp = api.model('auth_resp', {
+        'access_token': rest_fields.String
+    })
+    return resp
+
+def stream_put_resp(api):
+    resp = api.model('stream_put_resp', {
+        'message': rest_fields.String
+    })
+    return resp
