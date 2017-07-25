135548110b74a00f71f067917e282202e8a8e34c
nasirali1 <nasir.ali08@gmail.com>
2017-07-25 15:23:36 -0500

    Added models for response format

--- apiserver/__init__.py
+++ apiserver/__init__.py
@@ -28,4 +28,5 @@ from cerebralcortex.CerebralCortex import CerebralCortex
 
 configuration_file = os.path.join(os.path.dirname(__file__), '../cerebralcortex_apiserver.yml')
 
-CC = CerebralCortex(configuration_file, master="local[*]", name="Memphis cStress Development App", time_zone="US/Central")
+CC = CerebralCortex(configuration_file, master="local[*]", name="Memphis cStress Development App",
+                    time_zone="US/Central")
