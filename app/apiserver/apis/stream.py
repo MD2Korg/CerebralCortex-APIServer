98aa427d4c8bf77ebde5b501def20fcc60696f0b
Timothy W. Hnat <twhnat@memphis.edu>
2017-08-24 14:17:43 -0500

    Added the segmentation of CSV files into N-sized chuncks before sending to Kafka and organized the output json metadata into roughly the appropriate format

--- app/apiserver/apis/stream.py
+++ app/apiserver/apis/stream.py
@@ -27,12 +27,36 @@ import gzip
 from flask import request
 from flask_restplus import Namespace, Resource
 
+import json
+import datetime
+
 from .. import CC
 from ..core.data_models import stream_data_model, error_model, stream_put_resp
 from ..core.decorators import auth_required
 
 stream_route = CC.configuration['routes']['stream']
 stream_api = Namespace(stream_route, description='Data and annotation streams')
+
+def chunks(data, max_len):
+    """Yields max_len sized chunks with the remainder in the last"""
+    for i in range(0, len(data), max_len):
+        yield data[i:i+max_len]
+
+def datapoint(row):
+    """
+    Format data based on mCerebrum's current GZ-CSV format into what Cerebral
+    Cortex expects
+    """
+    ts,offset,values = row.split(',',2)
+    ts = int(ts)/1000.0
+    offset = int(offset)
+    values = list(map(float, values.split(',')))
+
+    timezone = datetime.timezone(datetime.timedelta(milliseconds=offset))
+    ts = datetime.datetime.fromtimestamp(ts,timezone)
+
+    return {'starttime': str(ts), 'value': values}
+
 
 
 @stream_api.route('/')
@@ -85,5 +109,12 @@ class Stream(Resource):
         gzip_file_content = gzip_file_content.decode('utf-8')
 
 
-        CC.kafka_produce_message("stream", gzip_file_content)
+        metadata_header = {'identifier': 'asdfkjladfaldf'}
+
+        lines = list(map(lambda x: datapoint(x), gzip_file_content.splitlines()))
+        for d in chunks(lines, 1000):
+            json_object = {'metadata': metadata_header, 'data': d}
+            # print(len(d), metadata_header)
+            CC.kafka_produce_message("stream", json.dumps(json_object))
+
         return {"message": "Data successfully received."}, 200
