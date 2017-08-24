7ec26bcc9c0d9be4ce7d7fa96434352c49415272
ali <ali@test.com>
2017-08-24 16:03:49 -0500

    fixed bad request error

--- test/locustfile.py
+++ test/locustfile.py
@@ -4,7 +4,7 @@ import json
 from os import listdir
 from os.path import isfile, join
 
-host = "http://127.0.0.1:8088/api/v1"
+host = "http://127.0.0.1:8088/api/v1/"
 data_dir = "/home/ali/IdeaProjects/MD2K_DATA/raw14/"
 
 
@@ -16,24 +16,18 @@ class LoadTestApiServer(TaskSet):
         self.login_api_server()
 
     def login_api_server(self):
-        self.client.headers['Content-Type'] = "application/json"
         payload = {"email_id":"string", "password":"string"}
-        response = self.client.post("/auth/", json=payload) #requests.post(url, payload)
+        response = self.client.post("auth/", json=payload) #requests.post(url, payload)
         json_response_dict = response.json()
         self.auth_token = json_response_dict["access_token"]
 
-    @task
+    @task(1)
     def put_zipped_stream(self):
-        self.client.headers['Content-Type'] = "multipart/form-data"
         self.client.headers['Authorization'] = self.auth_token
         onlyfiles = [f for f in listdir(data_dir) if isfile(join(data_dir, f))]
         for payload_file in onlyfiles:
             payload = dict(file=open(data_dir+payload_file, 'rb'))
-            print(payload)
-            self.client.put("/stream/zip/", data=payload)
-
-            #requests.put(host+"/stream/zip/", files=payload)
-
+            self.client.put("stream/zip/", files=payload)
 
 class WebsiteUser(HttpLocust):
     task_set = LoadTestApiServer
