f813540e36a0d1eeded6653b9d5bd965d073d25b
Timothy W. Hnat <twhnat@memphis.edu>
2017-08-25 16:38:49 -0500

    Added option to skip SSL validation, new gevent version requirement, and nginx configuration update

--- test/locustfile.py
+++ test/locustfile.py
@@ -5,10 +5,11 @@ from os import listdir
 from os.path import isfile, join
 
 #host = "http://127.0.0.1/api/v1"
+host = "https://127.0.0.1/api/v1"
 # host = "http://md2k-hnat/api/v1"
-host = "http://127.0.0.1:8088/api/v1"
-#data_dir = "gz/raw14/"
-data_dir = "/home/ali/IdeaProjects/MD2K_DATA/raw14/"
+# host = "http://127.0.0.1:8088/api/v1"
+data_dir = "gz/raw14/"
+# data_dir = "/home/ali/IdeaProjects/MD2K_DATA/raw14/"
 
 
 class LoadTestApiServer(TaskSet):
@@ -16,12 +17,14 @@ class LoadTestApiServer(TaskSet):
 
     def on_start(self):
         """ on_start is called when a Locust start before any task is scheduled """
+        # self.login_api_server()
+        self.client.verify = False
         pass
 
     @task(1)
     def api_flow(self):
         self.login_api_server()
-        self.put_zipped_stream
+        self.put_zipped_stream()
 
     def login_api_server(self):
         payload = {"email_id":"string", "password":"string"}
@@ -29,17 +32,18 @@ class LoadTestApiServer(TaskSet):
         json_response_dict = response.json()
         self.auth_token = json_response_dict["access_token"]
 
-
+    #@task(1)
     def put_zipped_stream(self):
         self.client.headers['Authorization'] = self.auth_token
         onlyfiles = [f for f in listdir(data_dir) if isfile(join(data_dir, f))]
-        for payload_file in onlyfiles:
+        for payload_file in onlyfiles[:1]:
             payload = dict(file=open(data_dir+payload_file, 'rb'))
             self.client.put("/stream/zip/", files=payload)
+            payload['file'].close()
 
 
 class WebsiteUser(HttpLocust):
     task_set = LoadTestApiServer
     host = host
-    min_wait = 500
+    min_wait = 1000
     max_wait = 1500
