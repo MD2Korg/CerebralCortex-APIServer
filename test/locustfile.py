5e20908b17422a4a1a5641b49d08ee674617c81f
nasirali1 <nasir.ali08@gmail.com>
2017-10-19 16:53:46 -0500

    removed commented code

--- test/locustfile.py
+++ test/locustfile.py
@@ -16,18 +16,6 @@ data_dir = "/home/ali/IdeaProjects/MD2K_DATA/CC_apiserver_files/"
 #host = "https://fourtytwo.md2k.org/api/v1"
 #data_dir = "gz/raw14/"
 
-# default_metadata = {
-#     "identifier": "0bf18489-bc04-42d9-8ded-dc54e686a67a",
-#     "name": "string",
-#     "data_descriptor": [
-#         {
-#             "unit": "string",
-#             "type": "string"
-#         }
-#     ],
-#     "owner": "7547cb22-c1a9-42ca-ac73-a7ddcc8a0a30",
-#     "execution_context": {}
-# }
 
 class LoadTestApiServer(TaskSet):
     auth_token = ""
