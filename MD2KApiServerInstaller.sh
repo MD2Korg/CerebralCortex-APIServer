fa5dee0b5961109521f237982ddce828d59ddb38
bhati416 <prashant.bhati@reancloud.com>
2018-02-08 15:25:45 +0530

    Update MD2KApiServerInstaller.sh
    added command to checkout perticular version of repo

--- MD2KApiServerInstaller.sh
+++ MD2KApiServerInstaller.sh
@@ -3,6 +3,7 @@ DB_ENDPOINT=$1
 sudo apt-get install python3-pip -y
 git clone https://github.com/MD2Korg/CerebralCortex
 cd CerebralCortex
+git checkout 50c34ae03dc99d8b9931173819002db5c3432c05
 pip3 install -r requirements.txt
 sudo python3 setup.py install
 cd ..
