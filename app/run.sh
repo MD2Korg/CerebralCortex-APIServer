aa297b27ec7c198dbb62c7b96dcacb4d5dbc1bc8
Anand Nandsat <anand@Anands-MacBook-Pro.local>
2018-03-09 16:28:08 -0600

    changed paths to match vagrant image

--- app/run.sh
+++ app/run.sh
@@ -4,13 +4,13 @@
 export PYSPARK_PYTHON=/usr/bin/python3
 
 # add current project root directory to pythonpath
-export PYTHONPATH="${PYTHONPATH}:/home/ali/IdeaProjects/CerebralCortex-2.0/"
+export PYTHONPATH="${PYTHONPATH}:/home/vagrant/CerebralCortex"
 
 
 # path of cc configuration path
-CC_CONFIG_FILEPATH="/home/ali/IdeaProjects/CerebralCortex-2.0/cerebralcortex/core/resources/cc_configuration.yml"
+CC_CONFIG_FILEPATH="/home/vagrant/IdeaProjects/CerebralCortex-DockerCompose/cc_config/cc_vagrant_configuration.yml"
 # data directory where all gz and json files will be stored
-DATA_DIR="/home/ali/IdeaProjects/MD2K_DATA/CC_APISERVER_DATA/"
+DATA_DIR="/home/vagrant/IdeaProjects/CerebralCortex-DockerCompose/data"
 
 
 python3 main.py -c $CC_CONFIG_FILEPATH -od $DATA_DIR
