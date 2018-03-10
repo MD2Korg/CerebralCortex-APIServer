0aa64cfa592200ae4d5526faec7192749d541d2f
Anand Nandsat <anand@Anands-MacBook-Pro.local>
2018-03-09 19:11:33 -0600

    changed run config

--- app/run.sh
+++ app/run.sh
@@ -4,13 +4,11 @@
 export PYSPARK_PYTHON=/usr/bin/python3
 
 # add current project root directory to pythonpath
-export PYTHONPATH="${PYTHONPATH}:/home/vagrant/CerebralCortex"
-
 
 # path of cc configuration path
-CC_CONFIG_FILEPATH="/home/vagrant/IdeaProjects/CerebralCortex-DockerCompose/cc_config/cc_vagrant_configuration.yml"
+CC_CONFIG_FILEPATH="/cc_config/cc_vagrant_configuration.yml"
 # data directory where all gz and json files will be stored
-DATA_DIR="/home/vagrant/IdeaProjects/CerebralCortex-DockerCompose/data"
+DATA_DIR="/data"
 
 
-python3 main.py -c $CC_CONFIG_FILEPATH -od $DATA_DIR
+python3 main.py -c $CC_CONFIG_FILEPATH -od $DATA_DIR -mf /merebrum_config
