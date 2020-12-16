4fe55a8aaf386206e9cef155e4d40ffa3100d5eb
nasirali1 <nasir.ali08@gmail.com>
2020-12-15 18:21:47 -0600

    changed offline data path (added hour)

--- app/apiserver/util/data_handler.py
+++ app/apiserver/util/data_handler.py
@@ -102,8 +102,9 @@ def store_data(stream_info: dict, user_settings: str, file: object, study_name, 
                 return {"status": False, "message": "Please check data_ingestion.yml file. parameters are not set properly."+str(raw_data_path)+" - does not exist."}
 
             try:
-                day = str(datetime.now().strftime('%d%m%y'))
-                output_folder_path = raw_data_path+"study="+study_name+"/stream="+stream_name+"/version="+str(stream_version)+"/user="+user_id+"/"+day+"/"
+                day = str(datetime.now().strftime('%Y%m%d'))
+                hour = str(datetime.now().strftime('%H'))
+                output_folder_path = raw_data_path+"study="+study_name+"/stream="+stream_name+"/version="+str(stream_version)+"/user="+user_id+"/"+day+"/"+hour+"/"
                 if not os.path.exists(output_folder_path):
                     os.makedirs(output_folder_path)
 
